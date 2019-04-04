013e7945be4365dcd79bc18f83868de7e81759ad
EvelynZhou <36960793+EvelynZhou@users.noreply.github.com>
2019-04-04 20:54:14 +0800

    Add files via upload

--- QueryResult.h
+++ QueryResult.h
@@ -1,12 +1,13 @@
 #pragma once
-#include "TextQuery.h"
-class QueryResult {
-	friend ostream& print(ostream&, const QueryResult&);
+#include "textquery.h"
+class QueryResult
+{
+	friend ostream &print(ostream &, const QueryResult &);
 public:
-	QueryResult(string s,shared_ptr<set<TextQuery::line_no>> p,shared_ptr<vector<string>> f):
-		sought(s),lines(p),file(f){ }
+	QueryResult(string s, shared_ptr<set<TextQuery::line_no>> p, shared_ptr<vector<string>> f) :
+		sought(s), lines(p), file(f) {}
 private:
-	string sought;
-	shared_ptr<set<TextQuery::line_no>> lines;
-	shared_ptr<vector<string>> file;
+	string sought;                                  //查询的单词
+	shared_ptr<set<TextQuery::line_no>> lines;      //包含的行数
+	shared_ptr<vector<string>> file;                //输入文件
 };
